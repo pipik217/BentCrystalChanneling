d94f7ad8e62917cdd612ed5a70305e0682497c7a
Eriator <omorgal12@gmail.com>
2025-06-03 20:08:30 +0200

    Add files via upload

--- propertiesNeutral.h
+++ propertiesNeutral.h
@@ -25,28 +25,28 @@ namespace Config {
 namespace silicium
     {
     // material properties Si(110)
-    constexpr double Uxc = 27e-9;            // GeV
-    constexpr double aTF = 0.148;             // Å
-    constexpr double d = 2.00;               // Å
-    constexpr double rho = 5.323;            // g/cm³
-    constexpr double Z = 32.0;
-    constexpr double A = 72.630;            // g/mol
-    constexpr double I = 350e-9;             // GeV
+    constexpr double d = 1.92;                 // Å 
+    constexpr double aTF = 0.194;             // Å
+    constexpr double Uxc = 16e-9;            // GeV
     constexpr double Uprime = 5.7;          // GeV/cm U'
+    constexpr double I = 173e-9;             // GeV         
+    constexpr double rho = 2.329;            // g/cm³
+    constexpr double A = 28.0855;            // g/mol    
+    constexpr double Z = 14.0;
     constexpr double xc{Config::calculateXc(silicium::d, silicium::aTF)};
     }
     
 namespace germanium
     {
     //material properties Ge(110)
-    constexpr double Uxc = 16e-9;            // GeV
-    constexpr double aTF = 0.194;             // Å
-    constexpr double d = 1.92;               // Å
-    constexpr double rho = 2.329;            // g/cm³
-    constexpr double Z = 14.0;
-    constexpr double A = 28.0855;            // g/mol
-    constexpr double I = 173e-9;             // GeV
+    constexpr double d = 2.00;   // Å 
+    constexpr double aTF = 0.148;             // Å
+    constexpr double Uxc = 27e-9;            // GeV
     constexpr double Uprime = 10.0;          // GeV/cm U'
+    constexpr double I = 350e-9;             // GeV         
+    constexpr double rho = 5.323;            // g/cm³
+    constexpr double A = 72.630;            // g/mol    
+    constexpr double Z = 32.0;        
     constexpr double xc{Config::calculateXc(germanium::d, germanium::aTF)};
     }
 
